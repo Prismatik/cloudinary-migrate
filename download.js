43aa18c7f877c2980ce57ef9088704e6e9dce87f
Simon Taylor <nizmox@gmail.com>
2015-07-13 17:27:07 +1000

    fix jshint errors

--- download.js
+++ download.js
@@ -1,59 +1,58 @@
 var Q = require('q');
 var config = require('root/config.json');
-var request = require('request')
-var qRequest = require('root/lib/q-request')
+var request = require('request');
+var qRequest = require('root/lib/q-request');
 
 //fetch all images from cloudinary (request 500 images repeatedly until no more are found)
 var fetchImages = function(opts, images) {
-	if (!images) images = []
-	//console.log('opts', opts)
+	if (!images) images = [];
 
 	return qRequest.request(opts)
 		.then(function(result) {
 			var newImages = result.body.resources;
-			var allImages = images.concat(newImages)
+			var allImages = images.concat(newImages);
 			console.log("new images found: ", newImages.length);
 			console.log("total images found: ", allImages.length);
 
 			if (result.body.next_cursor) {
-				console.log("there are more images to fetch!")
+				console.log("there are more images to fetch!");
 				
 				//update the next cursor in the opts
-				opts.qs.next_cursor = result.body.next_cursor
+				opts.qs.next_cursor = result.body.next_cursor;
 				//fetch the next lot of images
 				return fetchImages(opts, allImages);
 			}
 			return allImages;
 			
-		})
-}
+		});
+};
 
 var downloadImages = function(savePath, images) {
-	console.log("images remaining in batch", images.length)
+	console.log("images remaining in batch", images.length);
 
-	var image = images.splice(0,1)[0]
+	var image = images.splice(0,1)[0];
 	return qRequest.downloadFile(image.url, getFilename(image), savePath)
 		.then(function() {
-			if (images.length) downloadImages(savePath, images)
-		})
-}
+			if (images.length) downloadImages(savePath, images);
+		});
+};
 
 var getFilename = function(image) {
-	return image.public_id + "." + image.format
-}
+	return image.public_id + "." + image.format;
+};
 
 var getImagesURL = function(keys) {
 	var imagesURL = "https://" + keys.API_KEY + ":";
 	imagesURL += keys.API_SECRET + "@api.cloudinary.com/v1_1/";
 	imagesURL += keys.CLOUD_NAME + "/resources/image";
 	return imagesURL;
-}
+};
 
-console.log('getImagesURL', getImagesURL(config.from))
-var opts = {url: getImagesURL(config.from), qs: {max_results: 500}}
-var savePath = [__dirname, "images"].join("/")
-console.log('savePath', savePath)
+console.log('getImagesURL', getImagesURL(config.from));
+var opts = {url: getImagesURL(config.from), qs: {max_results: 500}};
+var savePath = [__dirname, "images"].join("/");
+console.log('savePath', savePath);
 
 fetchImages(opts)
 	.then(downloadImages.bind(this, savePath))
-	.catch(function(err) { console.error("error: ", err) })
+	.catch(function(err) { console.error("error: ", err); });
--- upload.js
+++ upload.js
@@ -2,58 +2,60 @@ var crypto = require('crypto');
 var fs = require('fs');
 var qs = require('querystring');
 
-var qRequest = require("root/lib/q-request")
-var config = require("root/config")
+var qRequest = require("root/lib/q-request");
+var config = require("root/config");
 
 var uploadImage = function(imagesDir, filename) {
-	console.log('uploading file', filename)
+	console.log('uploading file', filename);
 	var fullpath = [imagesDir, filename].join("/");
 
-	var now = new Date
-	var basename = filename.replace(/\.[^.]+$/, "")
+	var now = new Date();
+	var basename = filename.replace(/\.[^.]+$/, "");
 	
 	var query = {
 		public_id: basename,
 		timestamp: now.getTime()
-	}
+	};
 
 	//generate signature
-	var queryString = qs.stringify(query)
+	var queryString = qs.stringify(query);
 	var shasum = crypto.createHash('sha1');
 	shasum.update(queryString + config.upload.API_SECRET);
-	query.signature = shasum.digest('hex')
+	query.signature = shasum.digest('hex');
 
 	query.api_key = config.upload.API_KEY;
 
 	//read file into request
-	var formData = {file: fs.createReadStream(fullpath)}
+	var formData = {file: fs.createReadStream(fullpath)};
 
-	var url = "https://api.cloudinary.com/v1_1/" + config.upload.CLOUD_NAME + "/image/upload"
-	var opts = {url: url, qs: query, formData: formData}
+	var url = "https://api.cloudinary.com/v1_1/";
+	url += config.upload.CLOUD_NAME + "/image/upload";
+
+	var opts = {url: url, qs: query, formData: formData};
 
 	return qRequest.request(opts)
 		.then(function(result) {
 			var filename = result.body.original_filename + "." + result.body.format;
 			console.log('done uploading', filename);
 			return filename;
-		})
-}
+		});
+};
 
 var uploadImages = function(imagesDir) {
-	console.log("imagesDir", imagesDir)
+	console.log("imagesDir", imagesDir);
 
-	var files = fs.readdirSync(imagesDir)
+	var files = fs.readdirSync(imagesDir);
 
-	var firstFile = files.splice(0,1)[0]
-	var result = uploadImage(imagesDir, firstFile)
+	var firstFile = files.splice(0,1)[0];
+	var result = uploadImage(imagesDir, firstFile);
 	files.forEach(function(filename) {
 		//queue the next upload (use .bind to prevent immediate execution)
-		var nextUpload = uploadImage.bind(this, imagesDir, filename)
-		result = result.then(nextUpload)
-	})
+		var nextUpload = uploadImage.bind(this, imagesDir, filename);
+		result = result.then(nextUpload);
+	});
 	return result;
-}
+};
 
-var directory = __dirname + "/images"
+var directory = __dirname + "/images";
 uploadImages(directory)
-	.catch(function(err) { console.error(err) })
+	.catch(function(err) { console.error("error:", err); });
